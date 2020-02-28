20e929e4754b4754453b8b0d145ed75e6fc00c04
jorensorilla <joren.sorilla@gmail.com>
2020-02-28 23:29:49 +0800

    Revised popup.html to include jquery timepicker

--- content.js
+++ content.js
@@ -36,9 +36,76 @@ function stopRefresh(){
     clearInterval(pbiAutoRefresh)
 }
 
+function saveHandler() {
+    var interval = document.getElementById("refresh-interval").value
+    var interval_category = document.getElementById("interval-category").value
+    var interval_in_ms;
+    switch(interval_category) {
+        case "seconds": interval_in_ms = parseInt(interval*1000); 
+                        break;
+        case "minutes": interval_in_ms = parseInt(interval*1000*60);
+                        break;
+        case "hours": interval_in_ms = parseInt(interval*1000*60*60);  
+                      break;
+        default:
+            alert("Interval category invalid!");
+    }
+    chrome.runtime.sendMessage({directive:'save-event', reportinterval: interval_in_ms }, function(){
+        console.log("saveHandler");
+        
+    });
+}
+
+function refreshHandler() {
+    chrome.runtime.sendMessage({directive:'refresh-event'}, function(){
+        console.log("refreshHandler");
+        
+    });
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('save-button').addEventListener('click', saveHandler);
+    document.getElementById('refresh-button').addEventListener('click', refreshHandler);
+    // load config here
+    $('#interval-group').hide();
+    $('#time-group').hide();
+
+    $('#interval-radio').click(function () {
+        $('#interval-group').show();
+        $('#time-group').hide();
+
+    });
+
+    $('#time-radio').click(function () {
+        $('#interval-group').hide();
+        $('#time-group').show();
+
+    });
+
+    $(function () {
+        $('input.timepicker').timepicker({
+            timeFormat: 'h:mm p',
+            interval: 60,
+            minTime: '6',
+            maxTime: '10:00pm',
+            defaultTime: '7',
+            startTime: '6:00',
+            dynamic: false,
+            dropdown: true,
+            scrollbar: true
+        });
+    });
+    console.log("popup.js add event listener");
+})
+
+
+
 chrome.runtime.sendMessage({directive:'on-load-event'}, function(response){
-        alert(response.config[""])
+        alert(response.config["aaa"]);
 });
+
+
 console.log("Auto refresh started!");
 clickRefresh();
 
